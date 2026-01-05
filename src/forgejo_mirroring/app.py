23540374261328746d509e25bb2f2b433742a656
Ewilan Rivière <ewilan.riviere@gmail.com>
2026-01-05 11:24:51 +0100

    add commands to app

--- src/forgejo_mirroring/app.py
+++ src/forgejo_mirroring/app.py
@@ -1,10 +1,64 @@
-from forgejo_mirroring.repositories import GithubRepo, GitlabRepo, ForgejoRepo
+import sys
+import argparse
+from forgejo_mirroring.repositories import GithubRepo, GitlabRepo, ForgejoRepo, ForgeApi
 from forgejo_mirroring.logging import log
 
 
+def pythonCheck() -> None:
+    version_min = (3, 10)
+    if sys.version_info < version_min:
+        sys.stderr.write(
+            f"Error: Python {version_min[0]}.{version_min[1]} or later required.\n"
+        )
+        sys.exit(1)
+
+
+def parseArguments() -> tuple[bool, bool]:
+    parser = argparse.ArgumentParser(
+        description="Migrate repositories to Forgejo with mirroring"
+    )
+
+    help_delete = "Delete all mirroring repositories on Forgejo, before mirroring"
+    help_archived = "Mirroring archived repositories too"
+
+    parser.add_argument(
+        "-do",
+        "--delete",
+        action="store_true",
+        help=help_delete,
+    )
+    parser.add_argument(
+        "-a",
+        "--archived",
+        action="store_true",
+        help=help_archived,
+    )
+
+    args = parser.parse_args()
+
+    return bool(args.delete), bool(args.archived)
+
+
+def mirror_repositories(forge: ForgeApi, keep_archived: bool = False) -> ForgejoRepo:
+    forgejo = ForgejoRepo()
+    for repository in forge.repositories:
+        if keep_archived is not True and repository.archived:
+            log.warning(
+                f"Skip archived {repository.forge.value} {repository.full_name}"
+            )
+            continue
+
+        forgejo.mirror_repository(repository, forge.token)
+
+    return forgejo
+
+
 def main():
-    log.title("Forgejo Mirroring app started!")
+    log.title("Forgejo Mirroring")
     log.skip()
+
+    pythonCheck()
+    [delete, archived] = parseArguments()
 
     log.info("Parse GitHub repositories...")
     github = GithubRepo().listing()
@@ -17,3 +71,16 @@ def main():
     log.info(f"GitHub {len(github.repositories)} repositories")
     log.info(f"GitLab {len(gitlab.repositories)} repositories")
     log.info(f"Forgejo {len(forgejo.repositories)} repositories")
+
+    if delete:
+        log.skip()
+        mirrors = sum(1 for repo in forgejo.repositories if repo.mirrored)
+        log.info(f"Forgejo delete {mirrors} mirrors...")
+        forgejo.delete_mirrors()
+
+    log.skip()
+    log.info("Forgejo mirror GitHub repositories...")
+    mirror_repositories(github, archived)
+    log.info("Forgejo mirror GitLab repositories...")
+    mirror_repositories(gitlab, archived)
+    log.skip()
